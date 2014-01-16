5199c86f132e869124b283e1b7004a7feeacde7c
Z <1>
2014-01-16 15:13:39 +0800

    解决IE ajax缓存问题 FF不存在该问题

--- FineUploader/Controllers/HomeController.cs
+++ FineUploader/Controllers/HomeController.cs
@@ -19,12 +19,31 @@ namespace FineUploader.Controllers
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase qqfile)//qqfile是个固定的名称！
         {
+            if (qqfile != null && qqfile.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(qqfile.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data"), fileName);
+                try
+                {
+                    if (System.IO.File.Exists(path))
+                        System.IO.File.Delete(path);
+                    qqfile.SaveAs(path);
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, error = ex.Message }, "text/html");
+                }
+            }
             //qqfile.SaveAs(Server.MapPath("~/Upload/") + qqfile.FileName);
-            var filename = Path.Combine(Server.MapPath("~/App_Data"), qqfile.FileName);
-            qqfile.SaveAs(filename);
+            //var filename = Path.Combine(Server.MapPath("~/App_Data"), qqfile.FileName);
+            //qqfile.SaveAs(filename);
             //返回的json要写success=true，告诉fineuploader操作成功了。
             //http://stackoverflow.com/questions/16742116/fine-uploader-server-side-code-saves-uploaded-file-but-client-displays-upload
             return Json(new { success = true, name = qqfile.FileName }, "text/html");
+        }
+        public ActionResult Test()
+        {
+            return Content("hello world");
         }
     }
 }
